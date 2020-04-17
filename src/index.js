3e18a2677c6c109e3c603266fce083705fa2c76c
alight1 <andrelight.al@gmail.com>
2020-04-17 08:48:44 -0600

    Add favicon link

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 require("../src/style-common.css");
 require("../src/style.css");
 import bodyContent from "../src/body-content.html";
+import faviconPath from "../src/assets/company-logo-icon.png";
+
+const faviconLink = document.createElement("link");
+faviconLink.rel = "icon";
+faviconLink.href = faviconPath;
+document.head.appendChild(faviconLink);
 
 document.getElementById("body").innerHTML += bodyContent;
 
 //Append freeCodeCamp test utility (does not work when added directly to HTML)
 localStorage.setItem("project_selector", "product-landing-page");
-var fccScript = document.createElement("script");
+const fccScript = document.createElement("script");
 fccScript.src =
   "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
 document.head.appendChild(fccScript);
